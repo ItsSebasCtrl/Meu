affdea79adfa449f65ef1671dd6d5b91604c702b
Sebastian González <sebasguuuns@gmail.com>
2025-03-05 15:45:34 -0400

    Update Meu3.js

--- Meu3.js
+++ Meu3.js
@@ -1,7 +1,1 @@
-document.getElementById('back').addEventListener('click', function() {
-    window.history.back();
-});
 
-<h4>
-                <button id="forward"> Siguiente </button>
-            </h4>
