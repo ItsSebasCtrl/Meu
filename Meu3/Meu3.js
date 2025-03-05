9870f6b17700fa7775ed3de707c7c00ae447d2ac
Sebastian González <sebasguuuns@gmail.com>
2025-03-05 14:57:00 -0400

    Delete Meu3 directory

--- Meu3/Meu3.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('back').addEventListener('click', function() {
-    window.history.back();
-});
-
-<h4>
-                <button id="forward"> Siguiente </button>
-            </h4>
