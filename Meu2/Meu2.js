9977fdc01ff2402b8961435e43efe3816e34fd44
Sebastian González <sebasguuuns@gmail.com>
2025-03-05 14:54:16 -0400

    Add files via upload

---
+++ Meu2/Meu2.js
@@ -0,0 +1,3 @@
+document.getElementById('forward').addEventListener('click', function() {
+    window.location.href = "/Meu3/Meu3.html";
+});
---
+++ Meu3/Meu3.js
@@ -0,0 +1,7 @@
+document.getElementById('back').addEventListener('click', function() {
+    window.history.back();
+});
+
+<h4>
+                <button id="forward"> Siguiente </button>
+            </h4>
