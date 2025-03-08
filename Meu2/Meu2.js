398b9d2db1371b2dc4fa6c9dae19a17eb0ce6748
Sebastian González <sebasguuuns@gmail.com>
2025-03-08 10:25:24 -0400

    Update Meu2.js

--- Meu2/Meu2.js
+++ Meu2/Meu2.js
@@ -3,5 +3,5 @@ document.getElementById('forward').addEventListener('click', function() {
 });
 
 document.getElementById('back').addEventListener('click', function() {
-    window.location.href = "https://itssebasctrl.github.io/Meu/";
+    window.location.href = "https://itssebasctrl.github.io/Meu/index.html";
 });
