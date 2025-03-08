ddba0858e2591c697f6183bbb0edf2447c3011e5
Sebastian González <sebasguuuns@gmail.com>
2025-03-08 11:32:59 -0400

    Add

---
+++ Meu4/Meu4.js
@@ -0,0 +1,29 @@
+document.getElementById('back').addEventListener('click', function() {
+    window.location.href = "https://itssebasctrl.github.io/Meu/Meu3/Meu3.html";
+});
+
+document.getElementById('forward').addEventListener('click', function() {
+    window.location.href = "https://itssebasctrl.github.io/Meu/Meu5/Meu5.html";
+});
+
+document.getElementById('first').addEventListener('click', function() {
+    window.location.href = "/Sentimientos/Sentimiento-1/Sentimiento1.html";
+});
+
+document.getElementById('second').addEventListener('click', function() {
+    window.location.href = "/Sentimientos/Sentimiento-2/Sentimiento2.html";
+});
+
+document.getElementById('third').addEventListener('click', function() {
+    window.location.href = "/Sentimientos/Sentimiento-3/Sentimiento3.html";
+});
+
+document.getElementById('fourth').addEventListener('click', function() {
+    window.location.href = "/Sentimientos/Sentimiento-4/Sentimiento4.html";
+});
+
+document.getElementById('cloudybutton').addEventListener('click', function() {
+    window.location.href = "/Secreto/Secreto.html";
+});
+
+
