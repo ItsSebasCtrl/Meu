3d87d75873d1b4c542ff53a821285c6069115b13
Sebastian González <sebasguuuns@gmail.com>
2025-03-08 09:17:14 -0400

    Add files via upload

---
+++ Meu2/Meu2.js
@@ -0,0 +1,7 @@
+document.getElementById('forward').addEventListener('click', function() {
+    window.location.href = "/Meu3/Meu3.html";
+});
+
+document.getElementById('back').addEventListener('click', function() {
+    window.location.href = "/Meu.html";
+});
