8352003695b72c2739f0d55e7d5ed405ed4ce66d
Sebastian González <sebasguuuns@gmail.com>
2025-03-08 09:08:49 -0400

    Delete Meu2 directory

--- Meu2/Meu2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('forward').addEventListener('click', function() {
-    window.location.href = "/Meu3/Meu3.html";
-});
-
-document.getElementById('back').addEventListener('click', function() {
-    window.location.href = "/Meu.html";
-});
