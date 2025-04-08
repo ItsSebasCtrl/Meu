dba11da51003a18406d500b553e641f8227083b5
Sebastian González <sebasguuuns@gmail.com>
2025-04-08 18:47:35 -0400

    Add 8/4/2025

--- Secreto/Secreto.js
+++ Secreto/Secreto.js
@@ -1,3 +1,7 @@
+document.getElementById('forward').addEventListener('click', function() {
+    window.location.href = "https://itssebasctrl.github.io/Meu/Secreto/Secreto2.html";
+});
+
 document.getElementById('back').addEventListener('click', function() {
     window.location.href = "https://itssebasctrl.github.io/Meu/Meu4/Meu4.html";
 });
