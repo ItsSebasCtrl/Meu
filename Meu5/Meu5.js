5644ccf301c25487d6181fc836cdd2d05d6ae5ae
Sebastian González <sebasguuuns@gmail.com>
2025-04-10 18:53:03 -0400

    add

--- Meu5/Meu5.js
+++ Meu5/Meu5.js
@@ -9,3 +9,7 @@ document.getElementById('back').addEventListener('click', function() {
 document.getElementById('fifth').addEventListener('click', function() {
     window.location.href = "https://itssebasctrl.github.io/Meu/Sentimientos/Sentimiento-5/Sentimiento5.html";
 });
+
+document.getElementById('fifth').addEventListener('click', function() {
+    window.location.href = "https://itssebasctrl.github.io/Meu/Sentimientos/Sentimiento-6/Sentimiento6.html";
+});
