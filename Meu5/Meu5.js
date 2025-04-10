918b4064cfe957a392c777a48257bec2ddaa06b2
Sebastian González <sebasguuuns@gmail.com>
2025-04-10 18:27:49 -0400

    Add

--- Meu5/Meu5.js
+++ Meu5/Meu5.js
@@ -1,3 +1,7 @@
+document.getElementById('forward').addEventListener('click', function() {
+    window.location.href = "https://itssebasctrl.github.io/Meu/Meu6/Meu6.html";
+});
+
 document.getElementById('back').addEventListener('click', function() {
     window.location.href = "https://itssebasctrl.github.io/Meu/Meu4/Meu4.html";
 });
