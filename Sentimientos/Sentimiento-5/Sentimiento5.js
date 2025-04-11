c7952dac6bf6a824d8d9e47fc84f81d84e7081b5
Sebastian González <sebasguuuns@gmail.com>
2025-04-10 21:24:38 -0400

    add

--- Sentimientos/Sentimiento-5/Sentimiento5.js
+++ Sentimientos/Sentimiento-5/Sentimiento5.js
@@ -1,3 +1,3 @@
 document.getElementById('back').addEventListener('click', function() {
-    window.location.href = "https://itssebasctrl.github.io/Meu/Meu4/Meu4.html";
+    window.location.href = "https://itssebasctrl.github.io/Meu/Meu5/Meu5.html";
 });
