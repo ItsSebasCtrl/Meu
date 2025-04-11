b3f53414f645060ac6cb3a2b370204ff75679b96
Sebastian González <sebasguuuns@gmail.com>
2025-04-10 21:56:43 -0400

    add

--- Secreto/Secreto2/Secreto2.js
+++ Secreto/Secreto2/Secreto2.js
@@ -1,3 +1,7 @@
+document.getElementById('forward').addEventListener('click', function() {
+    window.location.href = "https://itssebasctrl.github.io/Meu/Secreto/Secreto3/Secreto3.html";
+});
+
 document.getElementById('back').addEventListener('click', function() {
     window.location.href = "https://itssebasctrl.github.io/Meu/Secreto/Secreto.html";
 });
